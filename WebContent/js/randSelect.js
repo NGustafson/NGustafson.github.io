d9bec4140137eb6f59fa956286fc5ee9c7e2c933
ngustafson2 <ngustafson2@ucmerced.edu>
2015-09-19 17:32:56 -0700

    index location change

---
+++ WebContent/js/randSelect.js
@@ -0,0 +1,9 @@
+angular.module('randomSelecter', [])
+	.controller('randomSelecterController', function() {
+		var randSelect = this;
+		randSelect.items = [];
+		
+		randSelect.addItem = function() {
+			//randSelect.items.push
+		}
+	});
